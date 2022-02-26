51e91d625f068b222b16c8fbd2a936a89759aed6
nobodyknowsdotcom <myclosestisa@gmail.com>
2022-02-27 01:09:57 +0500

    General functionality improvement

--- RsaLab/Program.cs
+++ RsaLab/Program.cs
@@ -1,119 +1,36 @@
 ﻿// https://vscode.ru/prog-lessons/algoritm-rsa.html
 
 using System;
-using System.Collections.Generic;
-using System.Numerics;
 
 namespace Rsa_lab
 {
     public class Program
     {
-        private readonly char[] _characters = new char[] 
-            {'#', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
-            'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 
-            'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
-            'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
-            '8', '9', '0'};
         public static void Main(string[] args)
         {
             Console.WriteLine("Введите простое число P:");
-            Rsa p = new Rsa(Int32.Parse(Console.ReadLine()));
+            var p = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Введите простое число Q:");
-            Rsa q = new Rsa(Int32.Parse(Console.ReadLine()));
+            var q = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine($"p:{p.Decode()}, q:{q.Decode()}");
-        }
-        
-        private List<string> RSA_Endode(string s, long e, long n)
-        {
-            List<string> result = new List<string>();
-            BigInteger bi;
-
-            for (int i = 0; i < s.Length; i++)
+            if (RSA.IsTheNumberSimple(p) && RSA.IsTheNumberSimple(q))
             {
-                int index = Array.IndexOf(_characters, s[i]);
-
-                bi = new BigInteger(index);
-                bi = BigInteger.Pow(bi, (int)e);
-
-                BigInteger n_ = new BigInteger((int)n);
-
-                bi = bi % n_;
-
-                result.Add(bi.ToString());
+                RSA rsaEncryptor = new RSA(p, q);
+                Console.WriteLine($"Ваши секретный ключи: \ne_:{rsaEncryptor.GetE_()} \nn:{rsaEncryptor.GetN()}");
+                
+                Console.WriteLine("Введите сообщение, которое будет зашифровано:");
+                var message = Console.ReadLine().ToUpper();
+                var encoded = rsaEncryptor.Encode(message);
+                Console.WriteLine("Зашифрованная строка:");
+                encoded.ForEach(Console.Write);
+                
+                Console.WriteLine("\nРасшифрованная строка:");
+                Console.WriteLine(rsaEncryptor.Decode(encoded));
             }
-
-            return result;
-        }
-    }
-
-    public class Rsa
-    {
-        private readonly byte[] _value;
-
-        public Rsa(int value)
-        {
-            _value = BitConverter.GetBytes(value);
-        }
-        
-        ~Rsa()
-        {
-            Console.WriteLine($"{Decode()} has been deleted");
-        }
-        
-        public byte[] GetValue()
-        {
-            return _value;
-        }
-
-        public int Decode()
-        {
-            return BitConverter.ToInt32(_value, 0);
-        }
-
-        public static Rsa operator +(Rsa value1, Rsa value2)
-        {
-            return new Rsa(value1.Decode() + value2.Decode());
-        }
-        
-        public static Rsa operator -(Rsa value1, Rsa value2)
-        {
-            return new Rsa(value1.Decode() - value2.Decode());
-        }
-
-        public static Rsa operator *(Rsa value1, Rsa value2)
-        {
-            return new Rsa(value1.Decode() * value2.Decode());
-        }
-
-        public static Rsa operator /(Rsa value1, Rsa value2)
-        {
-            return new Rsa(value1.Decode() / value2.Decode());
-        }
-
-        public static Rsa operator %(Rsa value1, Rsa value2)
-        {
-            return new Rsa(value1.Decode() % value2.Decode());
-        }
-
-        public static bool operator ==(Rsa value1, Rsa value2)
-        {
-            return value1.Decode() == value2.Decode();
-        }
-        
-        public static bool operator !=(Rsa value1, Rsa value2)
-        {
-            return value1.Decode() != value2.Decode();
-        }
-        
-        public static bool operator >(Rsa value1, Rsa value2)
-        {
-            return value1.Decode() > value2.Decode();
-        }
-        
-        public static bool operator <(Rsa value1, Rsa value2)
-        {
-            return value1.Decode() < value2.Decode();
+            else
+            {
+                Console.WriteLine("Пожалуйста, убедитесь в правильности введенных чисел (целочисленные и простые числа)");
+            }
         }
     }
 }
