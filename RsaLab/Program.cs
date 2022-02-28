3c6f83d92434b48338aab4c79a04fc8451674aa4
nobodyknowsdotcom <myclosestisa@gmail.com>
2022-03-01 00:59:15 +0500

    Make endless input

--- RsaLab/Program.cs
+++ RsaLab/Program.cs
@@ -9,42 +9,50 @@ namespace Rsa_lab
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Введите простое число P:");
-            var p = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Введите простое число Q:");
-            var q = Int32.Parse(Console.ReadLine());
+            var p = 0;
+            var q = 0;
             
-            if (RSA.IsTheNumberSimple(p) && RSA.IsTheNumberSimple(q))
+            while (true)
             {
-                var message = "";
-                StreamReader sr = new StreamReader("input.txt");
- 
-                while (!sr.EndOfStream)
+                Console.WriteLine("Введите простое число P:");
+                p = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Введите простое число Q:");
+                q = Int32.Parse(Console.ReadLine());
+
+                if (RSA.IsTheNumberSimple(p) && RSA.IsTheNumberSimple(q))
                 {
-                    message += sr.ReadLine();
+                    break;
                 }
- 
-                sr.Close();
- 
-                message = message.ToUpper();
-                
-                RSA rsaEncryptor = new RSA(p, q);
-                Console.WriteLine($"Ваши секретный ключи: \ne_:{rsaEncryptor.GetE_()} \nn:{rsaEncryptor.GetN()}");
-                
-                Console.WriteLine("Исходная строка:");
-                Console.WriteLine(message);
-                
-                var encoded = rsaEncryptor.Encode(message);
-                Console.WriteLine("Зашифрованная строка:");
-                encoded.ForEach(Console.Write);
-                
-                Console.WriteLine("\nРасшифрованная строка:");
-                Console.WriteLine(rsaEncryptor.Decode(encoded));
+                else
+                {
+                    Console.WriteLine(
+                        "Пожалуйста, убедитесь в правильности введенных чисел (целочисленные и простые числа)");
+                }
             }
-            else
+            
+            var message = "";
+            StreamReader sr = new StreamReader("input.txt");
+
+            while (!sr.EndOfStream)
             {
-                Console.WriteLine("Пожалуйста, убедитесь в правильности введенных чисел (целочисленные и простые числа)");
+                message += sr.ReadLine();
             }
+
+            sr.Close();
+
+            message = message.ToUpper();
+            RSA rsaEncryptor = new RSA(p, q);
+            Console.WriteLine($"Ваши секретный ключи: \ne_:{rsaEncryptor.GetE_()} \nn:{rsaEncryptor.GetN()}");
+            
+            Console.WriteLine("Исходная строка:");
+            Console.WriteLine(message);
+            
+            var encoded = rsaEncryptor.Encode(message);
+            Console.WriteLine("Зашифрованная строка:");
+            encoded.ForEach(Console.Write);
+            
+            Console.WriteLine("\nРасшифрованная строка:");
+            Console.WriteLine(rsaEncryptor.Decode(encoded));
         }
     }
 }
